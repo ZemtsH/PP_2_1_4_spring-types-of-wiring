876ad6443b3371a3fd3fbeaaadac477b5e7a6cb0
ZemtsH <zemtsovskaya.ov@gmail.com>
2023-09-22 16:59:36 +0300

    update AppConfig

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,6 @@
 package koschei.config;
 
+import koschei.KoscheiTheDeathless;
 import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,14 +9,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
+    @Bean
+    public KoscheiTheDeathless koscheiTheDeathless(Ocean1 ocean) {
+        KoscheiTheDeathless koscheiTheDeathless = new KoscheiTheDeathless();
+        koscheiTheDeathless.setOcean(ocean);
+        return koscheiTheDeathless;
+    }
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
 
     @Bean
-    public static Egg6 getEgg6(Needle7 needle) {
-        return new Egg6 (needle);
+    public Wood3 wood3(Rabbit4 rabbit) {
+        return new Wood3(rabbit);
+    }
+
+    @Bean
+    public Needle7 needle7() {
+        return new Needle7();
+    }
+
+    @Bean
+    public Egg6 egg6(Needle7 needle) {
+        return new Egg6(needle);
     }
 }
